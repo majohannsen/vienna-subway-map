584176648b0a1f2b88519a81f3fa4c94542b2039
Mathias Johannsen <mathias.j@arkulpa.at>
2023-12-17 13:18:47 +0100

    init, icon drives along line u3

---
+++ Path2D U3.gd
@@ -0,0 +1,26 @@
+extends Path2D
+
+var t := 0.0
+var direction = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if (direction):
+		t -= delta /10
+		if (t < 0):
+			t = 0
+			direction = !direction
+	else:
+		t += delta /10
+		if (t > 1):
+			t = 1
+			direction = !direction
+	
+	
+	
+	$PathFollow2D.progress_ratio = t
