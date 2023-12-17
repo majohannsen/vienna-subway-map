4fc9a61bdf64cb659711f9528420324ed98b446e
Mathias Johannsen <mathias.j@arkulpa.at>
2023-12-17 13:52:26 +0100

    random start and pixel based speed

--- PathFollow2D.gd
+++ PathFollow2D.gd
@@ -1,23 +1,24 @@
 extends PathFollow2D
 
-var t := 0.0
-var direction = false
+var t:float
+var direction:bool
+var speedFactor := 200
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	t = randf()*self.get_parent().get_curve().get_baked_length()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if (direction):
-		t -= delta /10
+		t -= delta * speedFactor
 		if (t < 0):
 			t = 0
 			direction = !direction
 	else:
-		t += delta /10
-		if (t > 1):
-			t = 1
+		t += delta * speedFactor
+		if (t > self.get_parent().get_curve().get_baked_length()):
+			t = self.get_parent().get_curve().get_baked_length()
 			direction = !direction
-	self.progress_ratio = t
+	self.progress = t
